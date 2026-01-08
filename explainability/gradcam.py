dd18be2f6cc00e4b94afdcc4cfff6c4bd9199b45
anshkumar0201 <anshkumar0201@gmail.com>
2026-01-08 09:13:25 +0530

    Create gradcam.py

---
+++ explainability/gradcam.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+def make_gradcam_heatmap(img_array, model, layer_name):
+    grad_model = tf.keras.models.Model(
+        model.inputs,
+        [model.get_layer(layer_name).output, model.output]
+    )
+
+    with tf.GradientTape() as tape:
+        conv_outputs, predictions = grad_model(img_array)
+        pred_index = tf.argmax(predictions[0])
+        class_channel = predictions[:, pred_index]
+
+    grads = tape.gradient(class_channel, conv_outputs)
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    conv_outputs = conv_outputs[0]
+    heatmap = conv_outputs @ pooled_grads[..., tf.newaxis]
+    heatmap = tf.squeeze(heatmap)
+    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+
+    return heatmap.numpy()
+
+def display_gradcam(image, heatmap, alpha=0.4):
+    heatmap = cv2.resize(heatmap, (image.shape[1], image.shape[0]))
+    heatmap = np.uint8(255 * heatmap)
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    overlay = heatmap * alpha + image
+
+    plt.imshow(overlay.astype("uint8"))
+    plt.axis("off")
+    plt.show()
