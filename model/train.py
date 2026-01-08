f454f0f47de3c4c3fd0145939d671eabcc5c94af
anshkumar0201 <anshkumar0201@gmail.com>
2026-01-08 09:21:45 +0530

    Update train.py

--- model/train.py
+++ model/train.py
@@ -1,13 +1,15 @@
 import numpy as np
 from tensorflow.image import resize_with_pad
-from build_model import build_model
+from .build_model import build_model
+
 
 def resize_images(images, size=224):
     return np.array([
         resize_with_pad(img, size, size) for img in images
     ]).astype("float32")
 
-def train_model(X_train, y_train, X_test, y_test):
+
+def train_model(X_train, y_train, X_test, y_test, epochs=50):
     model = build_model()
 
     X_train = resize_images(X_train * 255.0)
@@ -17,7 +19,8 @@ def train_model(X_train, y_train, X_test, y_test):
         X_train,
         y_train,
         validation_data=(X_test, y_test),
-        epochs=50
+        epochs=epochs
     )
 
-    model.save("trained_model.h5")
+    model.save("checkpoints/trained_model.h5")
+    return model
